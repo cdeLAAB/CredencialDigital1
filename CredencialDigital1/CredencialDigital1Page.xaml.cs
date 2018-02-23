9ce84b9d4a3d7058c126fd03cf48eadd2b2aa414
cdeLAAB <fany_21695@hotmail.com>
2018-02-22 20:10:47 -0600

    creacion inicial del projecto

---
+++ CredencialDigital1/CredencialDigital1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CredencialDigital1
+{
+    public partial class CredencialDigital1Page : ContentPage
+    {
+        public CredencialDigital1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
